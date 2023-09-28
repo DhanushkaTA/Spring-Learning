56db58a0250db2ef4654159589f1456547b37c89
DhanushkaTA <dhanushka999ta@gmail.com>
2023-09-28 16:31:47 +0530

    Introducing shutdown hook

--- _01_Core/src/main/java/lk/tda/springCore/AppInitializer.java
+++ _01_Core/src/main/java/lk/tda/springCore/AppInitializer.java
@@ -1,11 +1,21 @@
 package lk.tda.springCore;
 
 import lk.tda.springCore.bean.SpringBean;
+import lk.tda.springCore.bean.SpringBean2;
+import lk.tda.springCore.bean.SpringBean3;
 import lk.tda.springCore.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Jvm is shutdown");
+            }
+        }));
+
         AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
 
@@ -19,7 +29,15 @@ public class AppInitializer {
         bean.testBean();
         System.out.println(bean);
 
+        SpringBean2 bean2 = ctx.getBean(SpringBean2.class);
+        System.out.println("SpringBean2 (bean2) : "+bean2);
+
+        SpringBean3 bean3 = ctx.getBean(SpringBean3.class);
+        System.out.println("SpringBean3 (bean3) : "+bean3);
+
         ctx.close();
+
+
     }
 
 }
---
+++ _01_Core/src/main/java/lk/tda/springCore/bean/SpringBean3.java
@@ -0,0 +1,10 @@
+package lk.tda.springCore.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean3 {
+    public SpringBean3() {
+        System.out.println("SpringBean3 : "+this);
+    }
+}
