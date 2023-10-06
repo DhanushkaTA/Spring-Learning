7f49ea28834ebf2341eeb3f35c93ee4462578d30
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-07 01:33:59 +0530

    Update WebAppConfig class

--- _08_Spring_Web_MVC/src/main/java/lk/tda/SpringWebMVC/config/WebAppConfig.java
+++ _08_Spring_Web_MVC/src/main/java/lk/tda/SpringWebMVC/config/WebAppConfig.java
@@ -1,5 +1,13 @@
 package lk.tda.SpringWebMVC.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//AppConfig -> WebAppConfig
+@Configuration
+@ComponentScan(basePackages = "lk.tda.SpringWebMVC.bean")
+@EnableWebMvc
 public class WebAppConfig {
-    //AppConfig -> WebAppConfig
+
 }
