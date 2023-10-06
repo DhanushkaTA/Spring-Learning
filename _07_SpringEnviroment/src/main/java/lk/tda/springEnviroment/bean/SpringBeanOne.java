3da75760a1aea146bc67c1bfff37c00f2c833758
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-06 19:39:01 +0530

    Add Module 67  :- SpringEnviroment

---
+++ _07_SpringEnviroment/src/main/java/lk/tda/springEnviroment/bean/SpringBeanOne.java
@@ -0,0 +1,12 @@
+package lk.tda.springEnviroment.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanOne {
+    public SpringBeanOne() {
+        System.out.println("Bean Created");
+    }
+}
---
+++ _07_SpringEnviroment/src/main/java/lk/tda/springEnviroment/config/AppConfig.java
@@ -0,0 +1,9 @@
+package lk.tda.springEnviroment.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.tda.springEnviroment.bean")
+public class AppConfig {
+}
