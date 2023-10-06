05ed49f46222a38ba9bd11414d0f1a821519f70e
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-06 19:11:45 +0530

    UnsatisfiedDependencyException

--- _06_RunTimeValueInjection/src/main/java/lk/tda/RunTimeValueInjection/bean/SpringBeanOne.java
+++ _06_RunTimeValueInjection/src/main/java/lk/tda/RunTimeValueInjection/bean/SpringBeanOne.java
@@ -1,4 +1,21 @@
 package lk.tda.RunTimeValueInjection.bean;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringBeanOne {
+//    public SpringBeanOne() {
+//        System.out.println("Bean Created");
+//    }
+
+    //Error :- UnsatisfiedDependencyException
+    //Error creating bean with name 'springBeanOne' defined in file
+    // \[D:\JAVA\GDSE 63\Testing Apps\Spring\_06_RunTimeValueInjection\target\classes\lk\tda\RunTimeValueInjection\bean\SpringBeanOne.class]:
+    // Unsatisfied dependency expressed through constructor parameter 0; nested exception is
+    // org.springframework.beans.factory.NoSuchBeanDefinitionException:
+    // No qualifying bean of type 'java.lang.String' available: expected at least 1 bean which qualifies as autowire
+    // candidate. Dependency annotations: {}
+    public SpringBeanOne(String name) {
+        System.out.println("Bean Created");
+    }
 }
