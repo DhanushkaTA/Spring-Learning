f976cf4a1b338a0fcf8a8daa3f04cdf729e470f9
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-07 00:39:57 +0530

    Introduce Environment class to project

---
+++ _07_SpringEnviroment/src/main/java/lk/tda/springEnviroment/bean/DBConnection.java
@@ -0,0 +1,49 @@
+package lk.tda.springEnviroment.bean;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBConnection implements InitializingBean {
+
+    @Autowired
+    Environment environment;
+
+    @Value("${user.db.url}")
+    private String url;
+
+    @Value("${user.db.username}")
+    private String username;
+
+    @Value("${user.db.password}")
+    private String password;
+
+    @Value("${user.db.driverClassName}")
+    private String driverClassName;
+
+
+    public DBConnection() {
+        System.out.println("DBConnection Object Created");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(environment);
+
+        String username1 = environment.getProperty("USERNAME");
+        System.out.println(username1);
+
+        //wrong key -> null output
+        String username2 = environment.getProperty("USERNAMEe");
+        System.out.println(username2);
+
+        System.out.println(url);
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(driverClassName);
+        System.out.println("+++++++++++++++++++++++++ \n");
+    }
+}
