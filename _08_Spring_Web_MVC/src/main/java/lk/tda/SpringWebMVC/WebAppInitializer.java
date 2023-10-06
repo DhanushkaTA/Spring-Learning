c27081cb6d0270fd5c084828b092aa21f87d2359
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-07 01:29:52 +0530

    Update WebAppInitializer class

--- _08_Spring_Web_MVC/src/main/java/lk/tda/SpringWebMVC/WebAppInitializer.java
+++ _08_Spring_Web_MVC/src/main/java/lk/tda/SpringWebMVC/WebAppInitializer.java
@@ -1,8 +1,24 @@
 package lk.tda.SpringWebMVC;
 
-public class WebAppInitializer {
-    //AppInitializer -> WebAppInitializer
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import lk.tda.SpringWebMVC.config.WebAppConfig;
+import lk.tda.SpringWebMVC.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//AppInitializer -> WebAppInitializer
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
     }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
 }
