8f8e83fed11bf6b8a5b6c24e7d4bf766c7bd0baa
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-07 10:48:56 +0530

    Add Servlet API to Spring Web MVC project and run

--- _08_Spring_Web_MVC/src/main/java/lk/tda/SpringWebMVC/bean/SpringBean.java
+++ _08_Spring_Web_MVC/src/main/java/lk/tda/SpringWebMVC/bean/SpringBean.java
@@ -1,4 +1,39 @@
 package lk.tda.SpringWebMVC.bean;
 
-public class SpringBean {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    public SpringBean() {
+        System.out.println("SpringBean Created");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean Name Aware Called");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory Aware Called");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context Aware Called");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("SpringBean Ready to Use");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean Destroyed");
+    }
 }
