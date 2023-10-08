db1f7d77426d5a243276dc6f9f60c482ca1d7839
DhanushkaTA <dhanushka999ta@gmail.com>
2023-10-08 11:52:23 +0530

    Mapping/Handler Method Introduce

--- _09_RestController/src/main/java/lk/tda/RestController/controller/Controller.java
+++ _09_RestController/src/main/java/lk/tda/RestController/controller/Controller.java
@@ -1,4 +1,28 @@
 package lk.tda.RestController.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("controller")//Handler Mapping
 public class Controller {
+
+    @GetMapping//Handler Mapping
+    public String getMethod(){ // Handler/Mapping Method
+        return "Get Method invoked";
+    }
+
+    @PutMapping//Handler Mapping
+    public String putMethod(){ // Handler/Mapping Method
+        return "Put Method invoked";
+    }
+
+    @DeleteMapping//Handler Mapping
+    public String deleteMethod(){ // Handler/Mapping Method
+        return "Delete Method invoked";
+    }
+
+    @PostMapping//Handler Mapping
+    public String postMethod(){ // Handler/Mapping Method
+        return "Post Method invoked";
+    }
 }
